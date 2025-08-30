4ebbef504be1f65cba39be19c0722dfbb7e58453
Chirag Poornamath <chiragpoornamath@gmail.com>
2025-08-30 16:43:04 +0530

    Update api.ts

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -11,6 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ origin: true, credentials: true }));
+app.use(express.text({ type: "*/*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, _res, next) => {
